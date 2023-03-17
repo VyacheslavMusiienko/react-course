f148a9c7bb3aafc987744cd37398a505d71d94b4
VyacheslavMusiienko <chuvachok98@gmail.com>
2023-03-17 13:58:29 +0200

    init: react form

---
+++ react-vite-course/src/page/form-page.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Form from '../components/Form/Form';
+
+export default class FormPage extends React.Component {
+    render(): React.ReactNode {
+        return <Form />;
+    }
+}

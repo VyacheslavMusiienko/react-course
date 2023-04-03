6bd9540b500d065733e7204997a8409db6a09d54
VyacheslavMusiienko <chuvachok98@gmail.com>
2023-04-03 12:58:49 +0300

    fix: test inputCustom component

--- react-vite-course/src/components/Form/InputCustom/InputCustom.test.tsx
+++ react-vite-course/src/components/Form/InputCustom/InputCustom.test.tsx
@@ -1,18 +1,17 @@
-import React from 'react';
 import { render } from '@testing-library/react';
+import React from 'react';
 import { describe, it } from 'vitest';
 import InputCustom from './InputCustom';
 
 describe('InputCustom', () => {
     it('renders InputCustom component', () => {
         const surname: React.RefObject<HTMLInputElement> = React.createRef();
-        const surnameError = 'str';
+
         render(
             <InputCustom
                 title="Last Name:"
                 placeholder="Enter last name"
                 ref={surname}
-                errorMess={surnameError}
             />
         );
     });

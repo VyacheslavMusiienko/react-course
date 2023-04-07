4c13f482e429771a932dfec8d88d606a60348624
VyacheslavMusiienko <chuvachok98@gmail.com>
2023-04-07 17:57:01 +0300

    feat: test Cards

--- react-vite-course/src/components/Cards/Cards.test.tsx
+++ react-vite-course/src/components/Cards/Cards.test.tsx
@@ -1,17 +1,28 @@
-import { render } from '@testing-library/react';
-import { createMemoryRouter, RouterProvider } from 'react-router-dom';
-import { describe, it } from 'vitest';
+import { render, screen, waitFor } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import { describe, it, vi } from 'vitest';
 import Cards from './Cards';
 
 describe('Cards', () => {
-    const router = createMemoryRouter([
-        {
-            path: '/',
-            element: <Cards />,
-        },
-    ]);
-    it('renders Cards component', () => {
-        const { getByText } = render(<RouterProvider router={router} />);
-        expect(getByText('Loading...')).toBeInTheDocument();
+    it('renders Cards', () => {
+        render(
+            <BrowserRouter>
+                <Cards />
+            </BrowserRouter>
+        );
+        expect(screen.getByText('Loading...')).toBeInTheDocument();
+    });
+
+    it('should render an error message when there is an error', async () => {
+        const errorMessage = 'An error occurred';
+        vi.spyOn(global, 'fetch').mockRejectedValue(new Error(errorMessage));
+        render(
+            <BrowserRouter>
+                <Cards />
+            </BrowserRouter>
+        );
+        await waitFor(() =>
+            expect(screen.getByText(errorMessage)).toBeInTheDocument()
+        );
     });
 });

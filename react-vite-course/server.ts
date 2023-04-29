5b33e41bf549b9a6449793270b24c7b676279892
VyacheslavMusiienko <chuvachok98@gmail.com>
2023-04-29 14:36:17 +0300

    refactor: server file

--- react-vite-course/server.ts
+++ react-vite-course/server.ts
@@ -11,45 +11,45 @@ const indexHTML = path.resolve(__dirname, 'index.html');
 const PORT = process.env.PORT || 3001;
 
 async function startServer() {
-  const app = express();
-  const vite = await createServer({ server: { middlewareMode: true }, appType: 'custom' });
+    const app = express();
+    const vite = await createServer({ server: { middlewareMode: true }, appType: 'custom' });
 
-  app.use(vite.middlewares);
+    app.use(vite.middlewares);
 
-  app.use('*', async (request, response) => {
-    const url = request.originalUrl;
+    app.use('*', async (request, response) => {
+        const url = request.originalUrl;
 
-    try {
-      const template = fs.readFileSync(indexHTML, 'utf8');
-      const transformHTML = await vite.transformIndexHtml(url, template);
-      const [startHTML, endHTML] = transformHTML.split('<!--app-->');
+        try {
+            const template = fs.readFileSync(indexHTML, 'utf8');
+            const transformHTML = await vite.transformIndexHtml(url, template);
+            const [startHTML, endHTML] = transformHTML.split('<!--app-->');
 
-      const render = (await vite.ssrLoadModule('./src/entry-server.tsx')).render;
+            const render = (await vite.ssrLoadModule('./src/entry-server.tsx')).render;
 
-      try {
-        response.write(startHTML);
-        const stream = render(url, {
-          onShellReady() {
-            stream.pipe(response);
-          },
-          onAllReady() {
-            response.write(endHTML);
-            response.end();
-          },
-        });
-      } catch (error) {
-        console.error(error);
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  });
+            try {
+                response.write(startHTML);
+                const stream = render(url, {
+                    onShellReady() {
+                        stream.pipe(response);
+                    },
+                    onAllReady() {
+                        response.write(endHTML);
+                        response.end();
+                    },
+                });
+            } catch (error) {
+                console.error(error);
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    });
 
-  return app;
+    return app;
 }
 
 startServer().then((app) => {
-  app.listen(PORT, () => {
-    console.log(`Server is running >>> http://localhost:${PORT}`);
-  });
+    app.listen(PORT, () => {
+        console.log(`Server is running >>> http://localhost:${PORT}`);
+    });
 });
